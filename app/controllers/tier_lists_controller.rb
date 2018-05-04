1492b197ef6e8846fce8b98e4005499fafc55594
jkhoang313 <jkhoang313@gmail.com>
2018-05-03 20:32:15 -0400

    GET tier_list will respect offset and limit

--- app/controllers/tier_lists_controller.rb
+++ app/controllers/tier_lists_controller.rb
@@ -1,8 +1,14 @@
 class TierListsController < ApplicationController
   def index
-    list_type_id = params["list_type_id"].to_i
+    list_type_id = params["list_type_id"]
+    offset = params["offset"]
+    limit = params["limit"]
 
-    tier_lists = TierList.joins(:tier_list_types).where(tier_list_types: { id: list_type_id }).order(upvotes: :DESC)
+    if list_type_id
+      tier_lists = TierList.joins(:tier_list_types).where(tier_list_types: { id: list_type_id }).order(upvotes: :DESC).offset(offset).limit(limit)
+    else
+      tier_lists = TierList.order(upvotes: :DESC).offset(offset).limit(limit)
+    end
 
     render json: tier_lists, root: false
   end
