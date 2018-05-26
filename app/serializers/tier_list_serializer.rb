57dedcc46439da738b2c3eada88629591b9668f0
jkhoang313 <jkhoang313@gmail.com>
2018-05-25 22:59:25 -0400

    Added primary list type to tier list serializer

--- app/serializers/tier_list_serializer.rb
+++ app/serializers/tier_list_serializer.rb
@@ -2,5 +2,5 @@ class TierListSerializer < ActiveModel::Serializer
   has_one :user
   has_many :tier_list_types, :tiers
 
-  attributes :id, :title, :description, :upvotes
+  attributes :id, :title, :description, :upvotes, :primary_list_type_id
 end
